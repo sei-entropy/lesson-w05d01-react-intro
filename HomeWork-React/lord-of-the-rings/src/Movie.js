7ccf69fde897246a563b4d57bce88a4b853e9e10
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-02-16 22:15:31 +0300

    Movie Component

---
+++ HomeWork-React/lord-of-the-rings/src/Movie.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Movie extends Component {
+  render() {
+
+    return (
+      // we'll add JSX here
+      
+      <div>
+        <h1> The Lord of the Rings: { this.props.title } </h1>
+        <p> {this.props.hours}h {this.props.minutes}min </p>
+      </div>
+    
+    );
+  }
+}
+
+export default Movie;
